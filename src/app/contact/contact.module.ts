2799b07f047c508cd17bae6a5507d917564ffd47
Moe Zaky <devmuzaky@gmail.com>
2023-04-15 21:24:56 +0200

    refactoring style

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact/contact.component';
-import {MatIconModule} from "@angular/material/icon";
 
 
 
@@ -10,8 +9,7 @@ import {MatIconModule} from "@angular/material/icon";
     ContactComponent
   ],
   imports: [
-    CommonModule,
-    MatIconModule
+    CommonModule
   ]
 })
 export class ContactModule { }
