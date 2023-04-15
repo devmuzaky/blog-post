f6881ea51429872608f81b926c073148f0a53fd0
Moe Zaky <devmuzaky@gmail.com>
2023-04-15 22:31:24 +0200

    deploying...

--- scully.my-posts.config.ts
+++ scully.my-posts.config.ts
@@ -3,7 +3,7 @@ import {ScullyConfig} from '@scullyio/scully';
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "my-posts",
-  outDir: './static',
+  outDir: './dist/static',
   routes: {
     '/posts/:id': {
       type: 'contentFolder',
