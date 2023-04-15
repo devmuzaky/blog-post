78566959106d02740a1a2c5617168330f09370d2
Moe Zaky <devmuzaky@gmail.com>
2023-04-15 14:05:25 +0200

    installing Scully in the application

---
+++ scully.my-blog.config.ts
@@ -0,0 +1,8 @@
+import {ScullyConfig} from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "my-blog",
+  outDir: './dist/static',
+  routes: {}
+};
