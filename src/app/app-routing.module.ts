874512c154093c522278580718f51d10e611f519
Moe Zaky <devmuzaky@gmail.com>
2023-04-15 13:52:07 +0200

    configuring routing in the blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {ContactComponent} from "./contact/contact/contact.component";
 
-const routes: Routes = [
-  {
-    path: 'contact',
-    component: ContactComponent
-  },
-  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }
-];
+const routes: Routes = [{
+  path: 'contact', component: ContactComponent
+},{
+  path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule)
+}, {
+  path: '', pathMatch: 'full', redirectTo: 'articles'
+}, {
+  path: '**', redirectTo: 'articles'
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
